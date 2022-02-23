bc6a6efc8b885c492349a5454a6b2f42865e0738
Morgan Benton <morgan.benton@gmail.com>
2022-02-23 08:56:19 -0500

    replace pylint with black

--- units_converter_test.py
+++ units_converter_test.py
@@ -5,11 +5,11 @@ Unit tests for the MPG to KPL units converter
 # import the code to be tested
 from units_converter import mpg2kpl
 
-def describe_a_units_converter():
 
-  def that_can_convert_mpg_to_kpl():
-    """
-    Test the mpg2kpl function
-    """
-    assert mpg2kpl(30) == 12.7543
-    assert mpg2kpl(10) == 4.25144
+def describe_a_units_converter():
+    def that_can_convert_mpg_to_kpl():
+        """
+        Test the mpg2kpl function
+        """
+        assert mpg2kpl(30) == 12.7543
+        assert mpg2kpl(10) == 4.25144
